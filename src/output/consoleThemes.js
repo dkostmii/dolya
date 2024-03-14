00e8517d8374a37ff0f5efa3b06dfdcd49a7a817
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-03-14 16:38:56 +0200

    Change: Reimplementation
    Implement Dolya as Mavka pack.

--- src/output/consoleThemes.js
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-
-export const testFailStyle = chalk.bold.bgRed;
-export const testPassStyle = chalk.bold.bgGreen;
-export const testTitleStyle = chalk.bold;
-
-export const testGoodValue = chalk.green;
-export const testBadValue = chalk.red;
